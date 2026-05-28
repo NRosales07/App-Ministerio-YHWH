9c550d85db20a3b3654d0f849aee4d09a017d691
NRosales07 <rosalesjrnain07@gmail.com>
2026-05-28 14:54:45 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'alabanzas-v5';
+const CACHE_NAME = 'alabanzas-v6';
 // Lista de archivos que la app guardará en el teléfono para usarse sin internet
 const ASSETS = [
   'index.html',
